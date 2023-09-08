d8f60affa12eafaee6f8f1bfff179c4b752f8738
Leheza Oleksii <oleksii.leheza@gmail.com>
2023-09-08 21:36:59 +0300

    index already finished

--- script.js
+++ script.js
@@ -1,9 +1,98 @@
-const max_players = 5;
-const max_index = 3;
-var active_playres = 4;
-var max = 6;
+var max_index = 3; //change
+var max_players = 6;
+var active_playres;
+var kronos;
+var max = 6; ////////////////////
+
+document.addEventListener("DOMContentLoaded", function () {
+  const startButton = document.getElementById("startButton");
+  const playersRadios = document.querySelectorAll(
+    'input[name="radio-group-players"]'
+  );
+
+  function isPlayersSelected() {
+    for (const radio of playersRadios) {
+      if (radio.checked) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  startButton.addEventListener("click", function (event) {
+    if (!isPlayersSelected()) {
+      event.preventDefault();
+      alert("Будь ласка, виберіть кількість гравців перед початком.");
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const kronos_url = urlParams.get("kronos");
+  const kronosImage = document.getElementById("kronosImage");
+  if (kronos_url === "true") {
+    kronosImage.style.display = "block";
+  } else {
+    kronosImage.style.display = "none";
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const startButton = document.getElementById("startButton");
+  const playersRadios = document.querySelectorAll(
+    'input[name="radio-group-players"]'
+  );
+  const kronosCheckbox = document.getElementById("kronosCheckbox");
+
+  function updateStartButtonUrl() {
+    let url = "menu.html?";
+    const selectedPlayers = document.querySelector(
+      'input[name="radio-group-players"]:checked'
+    );
+
+    if (selectedPlayers) {
+      url += `players=${selectedPlayers.value}`;
+      active_playres = selectedPlayers.value;
+    }
+
+    if (kronosCheckbox.checked) {
+      if (selectedPlayers) {
+        url += "&";
+      }
+      url += "kronos=true";
+    } else {
+      if (selectedPlayers) {
+        url += "&";
+      }
+      url += "kronos=false";
+    }
+
+    startButton.href = url;
+  }
+
+  playersRadios.forEach(function (radio) {
+    radio.addEventListener("change", updateStartButtonUrl);
+  });
+
+  kronosCheckbox.addEventListener("change", updateStartButtonUrl);
+  updateStartButtonUrl();
+});
+
+
 
 function shuffle_gods() {
+  const urlParams = new URLSearchParams(window.location.search);
+  if (urlParams.get("players")) {
+    active_playres = urlParams.get("players");
+  }
+
+  kronos = urlParams.get("kronos");
+  if (kronos === true) {
+    max_players = 6;
+  } else {
+    max_players = 5;
+  }
   let gods_images = get_list_gods();
   shuffle(gods_images);
 }
